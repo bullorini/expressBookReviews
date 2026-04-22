a05957879884baf5c8b5fe64a85ad4121a45c57f
Cristian Bullorini <cristian@microblet.com>
2026-04-22 14:02:17 -0300

    do tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,128 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+   res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const matchedBooks = {};
+
+  bookKeys.forEach((key) => {
+    if (books[key].author.toLowerCase() === author) {
+      matchedBooks[key] = books[key];
+    }
+  });
+
+  if (Object.keys(matchedBooks).length > 0) {
+    return res.status(200).json(matchedBooks);
+  } else {
+    return res.status(404).json({
+      message: "No books found for the given author"
+    });
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const matchedBooks = {};
+
+  bookKeys.forEach((key) => {
+    if (books[key].title.toLowerCase() === title) {
+      matchedBooks[key] = books[key];
+    }
+  });
+
+  if (Object.keys(matchedBooks).length > 0) {
+    return res.status(200).json(matchedBooks);
+  } else {
+    return res.status(404).json({
+      message: "No books found for the given title"
+    });
+  }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  }
+
+  return res.status(404).json({
+    message: "Book not found"
+  });
 });
+
+// Axios with Promise callbacks - get books by title
+const getBooksByTitleUsingPromise = (title) => {
+  return axios
+    .get(`http://localhost:5000/title/${encodeURIComponent(title)}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// Axios with async-await - get books by title
+const getBooksByTitleUsingAsync = async (title) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/title/${encodeURIComponent(title)}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports.general = public_users;
+module.exports.getBooksByTitleUsingPromise = getBooksByTitleUsingPromise;
+module.exports.getBooksByTitleUsingAsync = getBooksByTitleUsingAsync;
